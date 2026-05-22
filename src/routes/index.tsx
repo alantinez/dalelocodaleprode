f2f91bd4e3f8e9c138ca2ed9c1ed5aba8d45b41e
alantinez <alanmartinezsl123@gmail.com>
2026-05-22 14:48:57 -0300

    Update index.tsx

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -3,7 +3,6 @@ import { Navbar } from "@/components/landing/Navbar";
 import { Hero } from "@/components/landing/Hero";
 import { PrizePool } from "@/components/landing/PrizePool";
 import { Features } from "@/components/landing/Features";
-import { Reglamento } from "@/components/landing/Reglamento";
 import { RankingPreview } from "@/components/landing/RankingPreview";
 import { CtaBand } from "@/components/landing/CtaBand";
 import { TransferimeRaton } from "@/components/landing/TransferimeRaton";
@@ -20,7 +19,6 @@ function Index() {
       <main>
 <Hero />
 <PrizePool />
-<Reglamento />
 <Features />
 <RankingPreview />
 <TransferimeRaton />
